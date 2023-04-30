2d026caabadeff3fd222976b8ea1881485b63e7f
eliott23 <62487535+eliott23@users.noreply.github.com>
2023-04-30 16:45:05 +0100

    clean

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -7,16 +7,21 @@ DiamondTrap::DiamondTrap()
     HitPoints = 100;
     EnergyPoints = 20;
     AttackDamage = 30;
+    std::cout << "DiamondTrap constructor called" << std::endl;
 }
 
 DiamondTrap::DiamondTrap(std::string n)
 {
     Name = n;
     ClapTrap::Name = n + "_clap_name";
+    HitPoints = 100;
+    EnergyPoints = 20;
+    AttackDamage = 30;
+    std::cout << "DiamondTrap constructor called" << std::endl;
 }
 
 void    DiamondTrap::whoAmi()
 {
-    std::cout << "Name = " << Name << "ClapTrap Name = " \
+    std::cout << "Name = " << Name << "; ClapTrap Name = " \
     << ClapTrap::Name  << std::endl;
 }
